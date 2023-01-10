05d82a2e5a6875903de27f1145cbf6c898bd7609
rootium <66018375+rootium@users.noreply.github.com>
2023-01-10 06:16:15 +0300

    Add files via upload

---
+++ draft/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            button {
+                cursor: pointer;
+            }
+        </style>
+        <title>mainy</title>
+    </head>
+    
+    <body>
+        <button id="buttonDraft">Draft</button>
+
+        <button id="buttonDog">Dog Tracker</button>
+
+        <button id="buttonQueue">Queue Timer</button>
+
+        <script src="home.js"></script>
+    </body>
+</html>
