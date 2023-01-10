eb7a2377ec4eb4ab37f8331804564bc4d8918126
rootium <66018375+rootium@users.noreply.github.com>
2023-01-10 05:24:45 +0200

    Delete result.js

--- draft/result.js
+++
@@ -1,8 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-var players = urlParams.get('players');
-
-players = players.split(",");
-
-const label = document.getElementById("label");
-label.textContent = players;
